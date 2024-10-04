a196e82fa0604b8cbad73e14f58391b26a285e3f
ALX1LK <109631463+ALX1LK@users.noreply.github.com>
2024-10-04 10:41:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('colorButton');
+const resetButton = document.getElementById('resetButton');
+
+button.addEventListener('click', () => {
+    const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFC300'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
+
+resetButton.addEventListener('click', function() {
+    document.body.style.backgroundColor = "";
+
+})
